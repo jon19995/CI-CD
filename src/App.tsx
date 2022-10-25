0c4d5dedf6f3f30a0481a9ff2a8a5f5fde8442cd
Evgenii <jon199995@gmail.com>
2022-10-25 20:15:06 +0300

    bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,11 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          class="App-link"
+
+
+
+
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -23,4 +27,4 @@ function App() {
   );
 }
 
-export default App;
+export default Appp;
